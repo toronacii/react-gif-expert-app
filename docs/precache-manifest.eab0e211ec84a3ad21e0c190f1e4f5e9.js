5917f0e895bc6f08a5563e7acb8a3cb47dc6b583
José Toro <toronacii@gmail.com>
2020-06-26 16:14:46 -0300

    docs added

---
+++ docs/precache-manifest.eab0e211ec84a3ad21e0c190f1e4f5e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37880ce5688c7d5e2122c9440d9ac440",
+    "url": "/index.html"
+  },
+  {
+    "revision": "93d23b2691caf57b4325",
+    "url": "/static/css/main.1f13a205.chunk.css"
+  },
+  {
+    "revision": "203def7a330cabd23492",
+    "url": "/static/js/2.1cc64cb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1cc64cb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93d23b2691caf57b4325",
+    "url": "/static/js/main.8804fcf0.chunk.js"
+  },
+  {
+    "revision": "6a26a6a45f9e65ecd26f",
+    "url": "/static/js/runtime-main.0ba3af82.js"
+  }
+]);
